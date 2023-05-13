ccd3a6a8c64ca74289a621ea2bef349cd58afdff
oktavianda11 <2110031802105@sar.ac.id>
2023-05-13 12:07:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajarintent/PindahActivity.kt
@@ -0,0 +1,12 @@
+package com.example.belajarintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class PindahActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pindah)
+    }
+}
